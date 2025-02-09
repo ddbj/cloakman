39d1c2678f76bb435a0335a168b838bfc1dadc59
Keita Urashima <ursm@ursm.jp>
2025-02-10 08:47:28 +0900

    Configure ACL

---
+++ openldap/docker-entrypoint-initdb.d/acl.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+. /opt/bitnami/scripts/libopenldap.sh
+trap ldap_stop EXIT
+ldap_start_bg
+
+cat <<EOF > /tmp/acl.ldif
+dn: olcDatabase={2}mdb,cn=config
+changetype: modify
+add: olcAccess
+olcAccess: {0}to * attrs=userPassword
+  by * auth
+olcAccess: {1}to dn.subtree="ou=users,dc=ddbj,dc=nig,dc=ac,dc=jp"
+  by dn.one="ou=services,dc=ddbj,dc=nig,dc=ac,dc=jp" read
+olcAccess: {2}to *
+  by * none
+EOF
+
+ldapmodify -a -Y EXTERNAL -H ldapi:/// -f /tmp/acl.ldif
+rm /tmp/acl.ldif
