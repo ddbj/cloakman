7c4f5ef9c2af017700aa924f6965b8b4a79ea452
Keita Urashima <ursm@ursm.jp>
2025-02-11 18:43:33 +0900

    Fix env name

--- app/models/ext_ldap.rb
+++ app/models/ext_ldap.rb
@@ -11,7 +11,7 @@ module ExtLDAP
 
         auth: {
           method:   :simple,
-          username: ENV.fetch("EXT_LDAP_USERNAME", "cn=admin,dc=example,dc=org"),
+          username: ENV.fetch("EXT_LDAP_BIND_DN",  "cn=admin,dc=example,dc=org"),
           password: ENV.fetch("EXT_LDAP_PASSWORD", "adminpassword")
         },
 
