f5f998069e1a332d87bab7eb4e649e16b71d4aeb
Keita Urashima <ursm@ursm.jp>
2025-02-22 21:39:28 +0900

    Refactor

--- openldap/docker-entrypoint-initdb.d/unique.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-. /opt/bitnami/scripts/libopenldap.sh
-trap ldap_stop EXIT
-ldap_start_bg
-
-ldapmodify -a -Y EXTERNAL -H ldapi:/// -f /docker-entrypoint-initdb.d/ldifs/unique.ldif
