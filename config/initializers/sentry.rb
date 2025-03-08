13b070491dd0760bdb973f18e766977578364d2d
Keita Urashima <ursm@ursm.jp>
2025-03-08 15:54:01 +0900

    Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,4 @@
 Sentry.init do |config|
-  config.breadcrumbs_logger = [ :active_support_logger ]
-  config.dsn                = Rails.application.config_for(:app).sentry_dsn
-  config.enable_tracing     = true
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+  config.dsn                = Rails.application.credentials.sentry_dsn
 end
