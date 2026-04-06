c52ff592c6d804fcd455b99c8aba2eb1c1e47e36
Keita Urashima <ursm@ursm.jp>
2026-04-06 18:57:51 +0900

    Avoid warning

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module Cloakman
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.time_zone = 'Asia/Tokyo'
+    config.active_storage.variant_processor = :disabled
+    config.time_zone                        = 'Asia/Tokyo'
   end
 end
